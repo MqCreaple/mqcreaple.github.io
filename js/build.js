50847479a383020caee825cd4212b26bc4c643f1
NYTxx <tom.geng@prismsus.org>
2021-07-22 17:16:14 +0800

    first test

--- js/build.js
+++ js/build.js
@@ -1,5 +1,30 @@
 $(document).ready(function() {
+
     $.getJSON("../config.json", function(config) {
-        $(".fromconig").append(config[$(this).name])
-    })
-})
+        /**
+         * Search for all patterns like {{key}} in the document
+         * and replace it with the corresponding value in config
+         * @param {Object} block 
+         */
+        function run(block) {
+            var text = $(block).html();
+            text = text.replace(/\{\{(\w+?)\}\}/gm, function(m, key, s, txt) {
+                return config[key];
+            });
+            $(block).html(text);
+        }
+
+        $("head").load("../include/head.html", function() {
+            $("title").append(
+                title + " | {{author}} - {{description}}"
+            );
+            run(this);
+        });
+        $("#header").load("../include/header.html", function() {
+            run(this);
+        });
+        $("#avatar").load("../include/avatar.html", function() {
+            run(this);
+        });
+    });
+});
