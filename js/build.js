0d1ccfc9e123085de57909849cd526556432ac84
NYTxx <tom.geng@prismsus.org>
2021-07-23 10:59:01 +0800

    add footer

--- js/build.js
+++ js/build.js
@@ -8,23 +8,33 @@ $(document).ready(function() {
          */
         function run(block) {
             var text = $(block).html();
-            text = text.replace(/\{\{(\w+?)\}\}/gm, function(m, key, s, txt) {
+            text = text.replace(/\{\{([\w\-]+?)\}\}/gm, function(m, key, s, txt) {
                 return config[key];
             });
             $(block).html(text);
         }
 
+        /*
+        load the head of the webpage, and #header, #avatar, #footer
+         */
         $("head").load("../include/head.html", function() {
             $("title").append(
                 title + " | {{author}} - {{description}}"
             );
             run(this);
         });
-        $("#header").load("../include/header.html", function() {
-            run(this);
-        });
-        $("#avatar").load("../include/avatar.html", function() {
-            run(this);
+        $.when(
+            $.get("../include/header.html", function(header) {
+                $("#container").prepend(header);
+            }),
+            $.get("../include/avatar.html", function(avatar) {
+                $("#right").append(avatar);
+            }),
+            $.get("../include/footer.html", function(footer) {
+                $("#container").append(footer);
+            })
+        ).done(function() {
+            run("#container");
         });
     });
 });
