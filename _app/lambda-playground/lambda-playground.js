bcb455176b87a67dc942e6fcc3242fb01d8d1bd9
MqCreaple <tom.geng@prismsus.org>
2022-08-28 11:33:42 -0400

    add blog lambda

--- _app/lambda-playground/lambda-playground.js
+++ _app/lambda-playground/lambda-playground.js
@@ -6,7 +6,18 @@ const replacingList = {
     "F": [ "λx", "λy", "y" ],
     "AND": [ "λa", "λb", [ "a", "b", "F" ] ],
     "OR":  [ "λa", "λb", [ "a", "T", "b" ] ],
-    "NOT": [ "λa", [ "a", "F", "T" ] ]
+    "NOT": [ "λa", [ "a", "F", "T" ] ],
+    "XOR": [ "λa", "λb", [ "a", [ "NOT", "b" ], "b" ] ],
+    // natural number
+    "0": [ "λf", "λx", "x" ],
+    "1": [ "λf", "λx", [ "f", "x" ] ],
+    "2": [ "λf", "λx", [ "f", [ "f", "x" ] ] ],
+    "3": [ "λf", "λx", [ "f", [ "f", [ "f", "x" ] ] ] ],
+    "4": [ "λf", "λx", [ "f", [ "f", [ "f", [ "f", "x" ] ] ] ] ],
+    "SUCC": [ "λn", "λf", "λx", [ "f", [ "n", "f", "x" ] ] ],
+    "+": [ "λm", "λn", "λf", "λx", [ "m", "f", [ "n", "f", "x" ] ] ],
+    "*": [ "λm", "λn", "λf", [ "m", [ "n", "f" ] ] ],
+    "PRED": [ "λn", "λf", "λx", [ [ "n", [ "λg", "λh", [ "h", [ "g", "f" ] ] ] , [ "λu", "x" ] ], "ID" ] ]
 }
 const lambdaExample1 = [ [ "λx", "λy", [ "+", "x", "y" ] ], "2", "3" ];
 const lambdaExample2 = [ "OR", [ "AND", "T", "F" ], ["T"] ];
@@ -54,19 +65,36 @@ function parseExpr(expr) {
 }
 
 /**
+ * Join key-value pairs of an object with given string.
+ * Idea from: https://stackoverflow.com/questions/68215522/simplest-way-to-join-object-key-value-pair-with
+ * @param obj object whose values are DOM nodes
+ * @param str string
+ * @returns joined string
+ */
+function joinObject(obj, str = " ") {
+    return Object.keys(obj).filter(key => obj[key]).map(key => `${key}=${obj[key].id}`).join(str);
+}
+
+function unjoinObject(sobj, str = " ") {
+    return Object.fromEntries(sobj.split(str).filter(value => (value != "")).map(str => {
+        let pair = str.split("=");
+        return [pair[0], document.getElementById(pair[1])];
+    }));
+}
+
+/**
  * Give each individual elements in the DOM tree a unique ID.
  * Add reference of variables to their lambda terms.
  * @param dom dom tree
  * @param globalName the name of whole expression
  * @param localName the name of the current node
  * @param refTable a list with pairs of variable names and corresponding lambda DOM nodes
- * @param definedVars set of defined variable names
  * @returns the dom tree
  */
-function nameExpr(dom, globalName, localName = "", refTable = {}, definedVars = []) {
+function nameExpr(dom, globalName, localName = "", refTable = {}) {
     dom.id = globalName + localName;
     // record defined variable names in DOM node's data section
-    dom.dataset.definedVars = definedVars.join(" ");
+    dom.dataset.refTable = joinObject(refTable);
 
     if(dom.classList.contains("l-atom")) {
         //* atom *//
@@ -85,16 +113,18 @@ function nameExpr(dom, globalName, localName = "", refTable = {}, definedVars = 
             child.dataset.refs = "";
             // add variable name to refTable
             refTable[child.innerHTML] = child;
-            // add variable to definedVars
-            definedVars.push(child.innerHTML);
         }
         // recursively name the body of lambda expression
-        nameExpr(dom.children[dom.children.length - 1], globalName, localName + "b", refTable, definedVars);
+        nameExpr(dom.children[dom.children.length - 1], globalName, localName + "b", refTable);
+        // remove entries from refTable
+        for(let i = 0; i < dom.children.length - 1; i++) {
+            delete refTable[dom.children[i].innerHTML];
+        }
     } else if(dom.classList.contains("l-func-call")) {
         //* function call *//
         for(let i = 0; i < dom.children.length; i++) {
             // recursively name the function and parameters
-            nameExpr(dom.children[i], globalName, localName + i, refTable, definedVars);
+            nameExpr(dom.children[i], globalName, localName + i, refTable);
         }
     }
     return dom;
@@ -160,7 +190,6 @@ function generateUnusedName(base, definedNames) {
  * @param definedVars already defined variable names
  */
 function renameExpr(dom, definedVars = []) {
-    dom.dataset.definedVars = definedVars.join(" ");
     if(dom.classList.contains("l-atom")) {
         return;
     }
@@ -196,14 +225,14 @@ function renderExpr(dom, replTable = {}) {
             dom.addEventListener('click', function(event) {
                 // when the element is clicked, replace the content of element to corresponding content in replacingList
                 let newExpr = parseExpr(replTable[dom.innerHTML]? replTable[dom.innerHTML]: replacingList[dom.innerHTML]);
-                dom.replaceWith(nameExpr(newExpr, dom.id));
-                let definedVars = dom.dataset.definedVars.split(" ");
-                renameExpr(newExpr, definedVars);
+                let refTable = unjoinObject(dom.dataset.refTable);
+                dom.replaceWith(nameExpr(newExpr, dom.id, "", refTable));
+                renameExpr(newExpr, Object.keys(refTable));
     
                 // Render the element.
                 // Only when its parent is a function call and the element is lambda expression, render its parent.
                 let nodeToRender = document.getElementById(dom.id);
-                if(nodeToRender.parentNode.classList.contains("l-func-call") && nodeToRender.classList.contains("l-lambda-expr")) {
+                if(nodeToRender.classList.contains("l-lambda-expr")) {
                     nodeToRender = nodeToRender.parentNode;
                 }
                 renderExpr(nodeToRender, replTable);
@@ -218,8 +247,20 @@ function renderExpr(dom, replTable = {}) {
         if(dom.children.length == 1) {
             let child = dom.children[0];
             dom.replaceWith(child);
-            (child.parentNode.classList.contains("l-func-call") && child.classList.contains("l-lambda-expr"))?
+            (child.classList.contains("l-lambda-expr"))?
                     renderExpr(child.parentNode, replTable): renderExpr(child, replTable);
+            return;
+        }
+        // case: lambda expression's body is another lambda expression
+        if(dom.children[dom.children.length - 1].classList.contains("l-lambda-expr")) {
+            let child = dom.children[dom.children.length - 1];
+            for(let i = 0; i < child.children.length - 1; i++) {
+                // merge lambda node of two expressions
+                dom.insertBefore(child.children[i].cloneNode(true), child);
+            }
+            child.replaceWith(child.children[child.children.length - 1]);
+            nameExpr(dom, dom.id, "", unjoinObject(dom.dataset.refTable));
+            renderExpr(dom, replTable);
             return;
         }
 
@@ -233,14 +274,14 @@ function renderExpr(dom, replTable = {}) {
         if(dom.children.length == 1) {
             let child = dom.children[0];
             dom.replaceWith(child);
-            (child.parentNode.classList.contains("l-func-call") && child.classList.contains("l-lambda-expr"))?
+            (child.classList.contains("l-lambda-expr"))?
                     renderExpr(child.parentNode, replTable): renderExpr(child, replTable);
             return;
         }
         // case: function is a lambda expression
         if(dom.children[0].classList.contains("l-lambda-expr")) {
             let lambdaExpr = dom.children[0];
-            let paramLen = Math.max(dom.children.length, lambdaExpr.children.length) - 1;
+            let paramLen = Math.min(dom.children.length, lambdaExpr.children.length) - 1;
             // loop through every function parameters and corresponding lambda nodes in the expression
             //* [   [ 'λx', 'λy', ... ],        a, b ]
             //*       lambda node         function parameter
@@ -269,15 +310,15 @@ function renderExpr(dom, replTable = {}) {
                     if(event.target.id == event.dataTransfer.getData("text")) {
                         let parent = event.target.parentNode;
                         let refs = event.target.dataset.refs.split(' ');
+                        let definedVars = Object.keys(unjoinObject(parent.children[parent.children.length - 1].dataset.refTable));
                         refs.forEach(refID => {
                             // loop through every node referenced by lambda node, replace them with the parameter
                             let refNode = document.getElementById(refID);
                             if(refNode) {
                                 let newNode = parameter.cloneNode(true);
-                                refNode.replaceWith(newNode);
-                                let definedVars = refNode.dataset.definedVars.split(" ");
+                                refNode.replaceWith(nameExpr(newNode, refNode.id, "", unjoinObject(dom.dataset.refTable)));
                                 if(definedVars) {
-                                    renameExpr(newNode, definedVars); // TODO
+                                    renameExpr(newNode, definedVars);
                                 }
                                 newNode.classList.remove("l-emph");
                             }
@@ -289,7 +330,7 @@ function renderExpr(dom, replTable = {}) {
                         if(parent.parentNode.classList.contains("l")) {
                             nodeToRender = parent.parentNode;
                         }
-                        renderExpr(nameExpr(nodeToRender, nodeToRender.id), replTable); // TODO (backup refTable)
+                        renderExpr(nameExpr(nodeToRender, nodeToRender.id, "", unjoinObject(nodeToRender.dataset.refTable)), replTable);
                     }
                 };
             }
@@ -312,13 +353,13 @@ function lambdaExpr(expr, name, panel, replTable = {}) {
 }
 
 function autoRender() {
-    $(document).ready(function() {
-        $("p.lambda-r").each(function(index) {
+    document.addEventListener("DOMContentLoaded", function() {
+        document.querySelectorAll("p.lambda-r").forEach(function(element, index) {
             let replTable = {};
-            if(this.dataset.replTable) {
-                replTable = JSON.parse(this.dataset.replTable);
+            if(element.dataset.replTable) {
+                replTable = JSON.parse(element.dataset.replTable);
             }
-            lambdaExpr(JSON.parse($(this).html()), `lexp${index}`, this, replTable);
+            lambdaExpr(JSON.parse(element.innerHTML), `lexp${index}`, element, replTable);
         });
     });
 }
